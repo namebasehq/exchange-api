11f1e8ece34f6c13baff1ac0f77a81393e7c9e91
Anthony Liu <a@anthony.ai>
2019-05-06 22:28:54 -0700

    Add a script to manually test that the endpoints work.

---
+++ test/testEndpoints.js
@@ -0,0 +1,32 @@
+const namebase = require('../lib/exchange');
+const credentials = require('../credentials.json');
+
+const x = new namebase.Exchange(
+  credentials.ACCESS_KEY,
+  credentials.SECRET_KEY,
+  credentials.API_ROOT
+);
+
+async function log(name, ...parameters) {
+  const result = await x[name].apply(x, parameters);
+  console.log(`--- Executing ${name}() ---\n`);
+  console.log(result);
+  console.log('\n\n');
+}
+
+(async () => {
+  await log('info');
+  await log('depth', namebase.HNSBTC);
+  await log('priceTicker', namebase.HNSBTC);
+  await log('bookTicker', namebase.HNSBTC);
+  await log('klinesTicker', namebase.HNSBTC, namebase.ONE_HOUR, null, null, 2);
+  await log('dayTicker', namebase.HNSBTC);
+  await log('trades', namebase.HNSBTC, null, 2);
+  await log('openOrders', namebase.HNSBTC);
+  await log('allOrders', namebase.HNSBTC, null, 2);
+  await log('accountInfo');
+  await log('userTrades', namebase.HNSBTC, null, 2);
+  await log('depositAddress', namebase.HNS);
+  await log('depositHistory', namebase.HNS);
+  await log('withdrawalHistory', namebase.BTC);
+})();
